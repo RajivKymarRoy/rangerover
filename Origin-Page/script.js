51405ca7821421683311c1e247434f6ee6d7e564
Rajiv Kumar <mahirjv@gmail.com>
2023-09-15 09:43:44 +0530

    origin page completion

---
+++ Origin-Page/script.js
@@ -0,0 +1,12 @@
+// MOVEABLE & FADE PART 
+$(".moveable-pic1").animate({
+    "margin-left": "76%"
+}, 2000);
+
+$(".fadein-fadeout").fadeOut(4000);
+
+
+$(".moveable-pic2").animate({
+    "margin-right": "76%"
+}, 2000);
+
